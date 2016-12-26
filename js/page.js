0909063d4bff095bf5daefcabe19bf2069f8f6ba
Neeraj Pant <neeraj.pant@nvish.com>
2016-12-26 18:26:15 +0530

    image url and flow change

--- js/page.js
+++ js/page.js
@@ -6,17 +6,18 @@
     var custom_opt = document.getElementById('custom-wrapper');
 
     var page_wraper = document.getElementsByClassName('back-page')[0];
-
     var new_image_id = Math.floor(Math.random() * 1084);
+
     var old_images_arr = [];
     var is_first = false;
     var is_next_day = false;
+
 
     chrome.storage.sync.get("selected_background", function (obj) {
         selected_background = obj.selected_background;
         switch (selected_background) {
             case 'google':
-                google_opt.style.display = 'block';
+                window.location.href = "https://www.google.com";
                 break;
             case 'custom':
                 custom_opt.style.display = 'block';
@@ -32,34 +33,30 @@
 
 
     function customImagePageInit() {
-    	// Clear Evert Stored Data
+    	
+        // Clear Every Stored Data
  		
  		/*
-    	chrome.storage.sync.remove('stored_image_arr', function (obj) {alert('removed stored images');});
     	chrome.storage.sync.remove('thumb_images', function (obj) {alert('removed thumb images');});
     	chrome.storage.sync.remove('page_background_image', function (obj) {alert('removed background image');});
     	chrome.storage.sync.remove('image_change_time', function (obj) {alert('removed Image Change Time');});
+        localStorage.clear();
     	return;
-    	*/
+        //*/
+        
 
-		searchGoogle();
-		selectedPageBackground()
+    	
+		selectedPageBackground();
 		getImageIds();
-    }
-
-    function searchGoogle() {
-        var search_box = document.getElementsByClassName('google-search-submit')[0];
-
-        search_box.addEventListener('click', function () {
-            window.location.href = "https://www.google.com/search?q=" + this.value;
-        })
+        createImageThumbs();
     }
 
 
     function selectedPageBackground() {
-        chrome.storage.sync.get("page_background_image", function (obj) {
+        chrome.storage.sync.get('page_background_image', function(obj){
             if (obj.page_background_image != undefined) {
-                page_wraper.style.backgroundImage = 'url(' + obj.page_background_image + ')';
+                var url = localStorage.getItem(obj.page_background_image);
+                page_wraper.style.backgroundImage = 'url(' + url + ')';
             } else {
                 page_wraper.style.backgroundImage = 'url(images/back1.png)';
             }
@@ -68,21 +65,6 @@
 
 
     function getImageIds() {
-        chrome.storage.sync.get('stored_image_arr', function (obj) {
-            // console.log(obj)
-            if (obj.stored_image_arr != undefined && obj.stored_image_arr.length >= 1) {
-                for (var i = 0; i < obj.stored_image_arr.length; i++) {
-                    var new_item = obj.stored_image_arr[i];
-                    old_images_arr.push(new_item);
-                }
-            } else {
-                chrome.storage.sync.set({'stored_image_arr': old_images_arr}, function () {
-                });
-                is_first = true;
-            }
-        });
-
-
         chrome.storage.sync.get('image_change_time', function (obj) {
             if (obj.image_change_time != undefined) {
                 var now = getCurrentDate();
@@ -98,78 +80,50 @@
 
     function generateImageRandomIds() {
         if (is_next_day) {
-            // Images id from 706 - 754 is not available
-            var exclude_arr = [];
-            for (var z = 706; z <= 754; z++) {
-                exclude_arr.push(z);
-            }
-
-            var j = (is_first) ? 1 : 2;
-            while (j <= 3) {
-                if (!old_images_arr.includes(new_image_id) && new_image_id != 0 && !exclude_arr.includes(new_image_id)) {
-                    old_images_arr.push(new_image_id);
-                    j++;
-                }
-                new_image_id = Math.floor(Math.random() * 1084);
-            }
-
-            setTimeout(function () {
-                chrome.storage.sync.set({'stored_image_arr': old_images_arr}, function () {});
+            var url="https://source.unsplash.com/daily";
+            var convertFunction = convertFileToDataURLviaFileReader;
+            convertFunction(url, function(base64Img){
+                var index = localStorage.length;
+                localStorage.setItem(0, base64Img);
                 var date = getCurrentDate();
                 chrome.storage.sync.set({'image_change_time': date}, function () {});
-                setImageThumb();
-            }, 400);
-        }else{
-	        setImageThumb();
-	    }
+                generateNewThumbs();
+            });
+        }
     }
 
 
-
-    function setImageThumb() {
-        chrome.storage.sync.get('thumb_images', function (obj) {
-            var list_parent = document.getElementById('thumbs');
-            if (obj.thumb_images != undefined && obj.thumb_images.length != 0) {
-                for (var i = 0; i <= 2; i++) {
-                    var list = document.createElement('li');
-                    var thumb_img = obj.thumb_images[i][0];
-                    var background = "https://unsplash.it/1366/768?image=" + obj.thumb_images[i][1];
-                    var new_thumb = "<input type='radio' class='img-option' id='ext-image-" + i + "' name='image-select' value='" + background + "'>"
-                        + " <label for='ext-image-" + i + "'>" + thumb_img + "</label>";
-                    list.innerHTML = new_thumb;
-                    list_parent.appendChild(list);
-                }
-            	changeBackgroundImage();
-            } else {
-                chrome.storage.sync.get('stored_image_arr', function (obj) {
-                	if(obj.stored_image_arr != undefined || obj.stored_image_arr.length != 0){
-	                    var display_arr = obj.stored_image_arr.reverse();
-	                    var thumbs_arr = [];
-	                    var win_height = window.innerHeight;
-	                    var win_width = window.innerWidth;
-
-	                    // https://source.unsplash.com/category/nature/1600x900
-	                    
-	                    for (var i = 0; i <= 2; i++) {
-	                        var list = document.createElement('li');
-	                        var thumb_img = "<img src='https://unsplash.it/200/200?image=" + display_arr[i] + "'>";
-	                        var background = "https://unsplash.it/"+win_width+"/"+win_height+"?image=" + display_arr[i];
-	                        var new_thumb = "<input type='radio' class='img-option' id='ext-image-" + i + "' name='image-select' value='" + background + "'>"
-	                            + " <label for='ext-image-" + i + "'>" + thumb_img + "</label>";
-	                        list.innerHTML = new_thumb;
-	                        list_parent.appendChild(list);
-	                        thumbs_arr.push([thumb_img, display_arr[i]]);
-	                    }
-	                    chrome.storage.sync.set({'thumb_images': thumbs_arr}, function () {
-	            			changeBackgroundImage();
-	                    });
-	                }
+    function generateNewThumbs() {
+        var url="https://source.unsplash.com/random/"+win_width+"x"+win_height;
+        var i = 1;
+        var arrange_thumb = function(){
+            if( i <= 2 ){
+                var convertFunction = convertFileToDataURLviaFileReader;
+                convertFunction(url, function(base64Img){
+                    localStorage.setItem(i, base64Img);
+                    i++;
+                    arrange_thumb();
                 });
+            }else{
+                createImageThumbs();
             }
-        });
-
+        }
+        arrange_thumb();
     }
 
+    function createImageThumbs() {
+        var list_parent = document.getElementById('thumbs');
+        for(var i = 0; i < localStorage.length; i++){
+            var list = document.createElement('li');
+            var thumb_img = localStorage.getItem(i);
+            var background = localStorage.key(i);
+            var new_thumb = "<input type='radio' class='img-option' id='ext-image-" + i + "' name='image-select' value='" + background + "'>"
+                + " <label for='ext-image-" + i + "'><img src='" + thumb_img + "'></label>";
+            list.innerHTML = new_thumb;
+            list_parent.appendChild(list);
+        }
+        changeBackgroundImage();
+    }
 
 
     function getCurrentDate() {
@@ -194,7 +148,10 @@
         var img_opt = document.getElementsByClassName('img-option');
         for (var i = 0; i < img_opt.length; i++) {
             img_opt[i].addEventListener('change', function () {
-                getImageBase64(this.value);
+                image_src = localStorage.getItem(this.value);
+                chrome.storage.sync.set({'page_background_image': this.value}, function () {
+                    page_wraper.style.backgroundImage = 'url(' + image_src + ')';
+                });
             });
         }
     }
@@ -213,18 +170,6 @@
 		xhr.open('GET', url);
 		xhr.send();
 	}
-
-	function getImageBase64(url){
-		var convertFunction = convertFileToDataURLviaFileReader;
-		convertFunction(url, function(base64Img) {
-			chrome.storage.sync.set({'page_background_image': base64Img}, function () {
-                page_wraper.style.backgroundImage = 'url(' + base64Img + ')';
-            });
-		});
-	}
-	
-	//getImageBase64();
-
 
 })()
 
